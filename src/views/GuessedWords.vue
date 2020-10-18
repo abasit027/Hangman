46cb643f7da15c27426a40e489749793e36c0a33
Abdul Basit <abasit027@gmail.com>
2020-10-18 16:29:17 +0500

    guessed word component and route added

---
+++ src/views/GuessedWords.vue
@@ -0,0 +1,34 @@
+<template>
+	<header>
+		<h1>Hangman</h1>
+		<p>Words correctly guessed</p>
+	</header>
+
+	<main>
+		<ul>
+			<li v-for="word in store.guessedWords" :key="word">{{ word }}</li>
+		</ul>
+	</main>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue';
+
+	import store from '@/store';
+
+	export default defineComponent({
+		name: 'GuessedWords',
+		data() {
+			return {
+				store: store.state,
+			};
+		},
+	});
+</script>
+
+<style lang="scss" scoped>
+	li {
+		list-style-type: none;
+		font-weight: 600;
+	}
+</style>
